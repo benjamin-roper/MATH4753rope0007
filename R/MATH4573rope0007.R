7e5c80581491ebad22bf5c12bf39079ea558e362
benjamin-roper <benjamin.j.roper-1@ou.edu>
2020-09-18 19:54:52 -0500

    Initial commit

---
+++ R/MATH4573rope0007.R
@@ -0,0 +1,10 @@
+#' BMI Package
+#'
+#'  Contains one function for creating BMI groups.
+#'
+#'  @docType package
+#'
+#'  @author Benjamin J Roper \email{benjamin.j.roper-1@ou.edu}
+#'
+#'  @name MATH4753rope0007
+#'  NULL
