b496cd654046ec7bda7c6bae11c39f0bbb6fb3c0
benjamin-roper <benjamin.j.roper-1@ou.edu>
2020-10-09 23:40:33 -0500

    1

---
+++ R/ddt.R
@@ -0,0 +1,16 @@
+#' DDT data set
+#'
+#' A data set from intro2R
+#'
+#' @format A data frame with 144 rows and 6 variables:
+#' \describe{
+#'   \item{RIVER}{river}
+#'   \item{MILE}{mile}
+#'   \item{SPECIES}{species}
+#'   \item{LENGTH}{length}
+#'   \item{WEIGHT}{weight}
+#'   \item{DDT}{ddt}
+#' }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"ddt"
